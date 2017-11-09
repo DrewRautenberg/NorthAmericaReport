e910b33cf85f324fce276d452c36a7395052365c
Drew Rautenberg <DREW3330@gmail.com>
2017-11-09 11:35:35 -0500

    Initial commit

---
+++ src/northamericareport/Country.java
@@ -0,0 +1,42 @@
+/* CLASS:  Country                    USED BY NorthAmericaReport PROGRAM
+ * AUTHOR: Drew Rautenberg            DESIGNER:  Dr. Kaminski
+ * DESCRIPTION:  This OOP class describes a SINGLE country.
+ *****************************************************************************/
+package northamericareport;
+
+public class Country {
+
+    //------------------------------------------------------ INSTANCE VARIABLES
+    // code, name, area, population - with CORRECT data types (not all Strings)
+
+
+    //---------------------------------------------------------- SETTER METHODS
+    // 1 for each instance variable, with this special EDITING:
+    // - remove the "INSERT INTO Country VALUES (" from front of code
+    // - trim surrounding ' marks on code and name
+    // - convert area & population (which come in as Strings) into int's
+
+
+
+
+    //---------------------------------------------------------- GETTER METHODS
+    // 1 for each instance variable, with this special TRANSLATING:
+    // - only the first 15 characters of name are returned
+
+
+
+    //----------------------------------------------------- CONSTRUCTOR METHODS
+    // not needed - "default constructor" automatically included   
+    //--------------------------------------------------------- toString METHOD
+    // Nicely formats data ready for printing (by main )
+    // - use getters rather than instance variables
+    // - use String.format to produce the string to be returned
+    // - the formatString does the following:
+    //      - 2 numeric fields are right-justified with embedded commas
+    //      - 2 alphabetic fields are left-justified
+
+
+
+
+
+}
---
+++ src/northamericareport/NorthAmericaReport.java
@@ -0,0 +1,68 @@
+/* PROJECT:  NorthAmericaReport
+ * AUTHOR:   Drew Rautenberg          DESIGNER:  Dr. Kaminski
+ * DESCRIPTION:  Program reads CountryData.csv file,
+ *      - selects just the countries in North America - field[3] is continent
+ *      - stores them in an ARRAY OF OBJECTS.
+ *      - saves just 4 fields for country:  code, name, area, population
+ *              (fields 0, 2, 5, 7).
+ * NOTE:  Some cleanup and conversion of the data is needed, which is done in
+ *        the SETTER methods in Country class.
+ * OUTPUT:  Program prints a simple report to a FILE, using the toString method
+ *          (inside Country object) to nicely format each detail line.
+ *****************************************************************************/
+package northamericareport;
+import java.io.*;
+import java.util.Scanner;
+
+public class NorthAmericaReport {
+
+    public static void main(String[] args) throws IOException {
+        //-------------------------------------------------------- DECLARATIONS
+        final int MAX_N = 100;
+        int n = 0;
+        String record;
+        String[] field = new String[20];
+        String continent;
+
+        // Declare array of MAX_N Country OBJECTS
+
+
+
+        //-------------------------------------- FILL ARRAY WITH DATA FROM FILE
+        File theFile = new File("CountryData.csv");
+        Scanner inFile = new Scanner(theFile);
+
+        int i = 0;
+        while (inFile.hasNext()) {
+            record = inFile.nextLine();
+            field = record.split(",");
+            continent = field[3].replaceAll("\'", "");
+            if (continent.compareTo("North America") == 0) {
+                // declare ONE OBJECT (next element in the array) using [i]
+
+
+                // call 4 setters with RAW DATA fields 0,2,5,7
+                // - still as "dirty data" & still as strings)
+                // - cleaning & converting done in setters
+
+
+
+
+                i++;
+            }
+        }
+        n = i;
+        System.out.printf("FYI: n is %d\n\n", n);
+        inFile.close();
+        //---------------------------------------- WRITE REPORT FROM ARRAY DATA
+        // Write to FILE.  Use toString method (in Country class) to produce a
+        //      detail line.  (No header lines needed).
+
+
+
+
+
+
+        System.out.println("See NorthAmericaReport.txt file in project folder");
+    }
+}
