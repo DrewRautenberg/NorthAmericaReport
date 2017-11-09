174dd27ba3f3896606be28aa6d06ad45ac2bd056
Drew Rautenberg <DREW3330@gmail.com>
2017-11-09 11:47:23 -0500

    set methods

--- src/northamericareport/Country.java
+++ src/northamericareport/Country.java
@@ -6,37 +6,51 @@ package northamericareport;
 
 public class Country {
 
-    //------------------------------------------------------ INSTANCE VARIABLES
+    // INSTANCE VARIABLES
     // code, name, area, population - with CORRECT data types (not all Strings)
 
+    private String code;
+    private String name;
+    private int area;
+    private int population;
 
-    //---------------------------------------------------------- SETTER METHODS
+    // SETTER METHODS
     // 1 for each instance variable, with this special EDITING:
     // - remove the "INSERT INTO Country VALUES (" from front of code
     // - trim surrounding ' marks on code and name
     // - convert area & population (which come in as Strings) into int's
 
+    void setCode(String rCode) {
+        code = rCode.substring(29, 32);
+    }
 
+    void setName(String rName) {
+        name = rName.substring(1, rName.length() - 1);
+    }
 
+    void setArea(String rArea) {
+        area = Integer.parseInt(rArea);
+    }
 
-    //---------------------------------------------------------- GETTER METHODS
+    void setPopulation(String rPop) {
+        population = Integer.parseInt(rPop);
+    }
+
+    // GETTER METHODS
     // 1 for each instance variable, with this special TRANSLATING:
     // - only the first 15 characters of name are returned
 
 
+    // CONSTRUCTOR METHODS
+    // not needed - "default constructor" automatically included
 
-    //----------------------------------------------------- CONSTRUCTOR METHODS
-    // not needed - "default constructor" automatically included   
-    //--------------------------------------------------------- toString METHOD
+    // toString METHOD
     // Nicely formats data ready for printing (by main )
     // - use getters rather than instance variables
     // - use String.format to produce the string to be returned
     // - the formatString does the following:
     //      - 2 numeric fields are right-justified with embedded commas
     //      - 2 alphabetic fields are left-justified
-
-
-
 
 
 }
